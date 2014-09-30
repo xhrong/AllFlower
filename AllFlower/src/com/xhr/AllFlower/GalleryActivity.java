b71e0674c0958e68de57c479600ad044ebb53199
xianhai427 <et_rxh@163.com>
2014-09-30 14:08:04 +0800

    删除APK ---xhrong

--- AllFlower/src/com/xhr/AllFlower/GalleryActivity.java
+++ AllFlower/src/com/xhr/AllFlower/GalleryActivity.java
@@ -1,12 +1,17 @@
 package com.xhr.AllFlower;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.iflytek.iFramework.http.asynhttp.AsyncHttpClient;
 import com.iflytek.iFramework.http.asynhttp.AsyncHttpResponseHandler;
@@ -17,6 +22,7 @@ import com.xhr.AllFlower.model.ImageInfo;
 import com.xhr.AllFlower.utils.ImageDownloader;
 import org.apache.http.Header;
 
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -34,7 +40,7 @@ public class GalleryActivity extends Activity {
     private List<String> imageUrlList = new ArrayList<String>();
 
     private ImageView ivDownload;
-    private TextView tvImageDescription;
+    private ImageButton imgBtnShiTu;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,7 +56,6 @@ public class GalleryActivity extends Activity {
             @Override
             public void onItemChange(int currentPosition) {
                 currPos = currentPosition;
-           //     tvImageDescription.setText(imageInfoList.get(currPos).getDescription());
             }
         });
         mViewPager = (GalleryViewPager) findViewById(R.id.gvImageViewer);
@@ -71,8 +76,13 @@ public class GalleryActivity extends Activity {
                 ImageDownloader.getInstance().downloadFile(imageInfoList.get(currPos).getUrl());
             }
         });
-      //  tvImageDescription = (TextView) findViewById(R.id.tvImageDescription);
-    //    tvImageDescription.setText(imageInfoList.get(currPos).getDescription());
+        imgBtnShiTu = (ImageButton) findViewById(R.id.imgBtnShiTu);
+        imgBtnShiTu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shiTu(imageInfoList.get(currPos).getUrl());
+            }
+        });
     }
 
 
@@ -88,76 +98,36 @@ public class GalleryActivity extends Activity {
 
     private void DescriptionAnim() {
         Animation animation;
-        if (tvImageDescription.getVisibility() == View.VISIBLE) {
-            animation = AnimationUtils.loadAnimation(this, R.anim.gallery_description_hide);
-
-            animation.setAnimationListener(new Animation.AnimationListener() {
-                @Override
-                public void onAnimationStart(Animation animation) {
-                }
-
-                @Override
-                public void onAnimationEnd(Animation animation) {
-                    tvImageDescription.setVisibility(View.INVISIBLE);
-                }
-
-                @Override
-                public void onAnimationRepeat(Animation animation) {
-
-                }
-            });
-            tvImageDescription.startAnimation(animation);
-        } else {
-            animation = AnimationUtils.loadAnimation(this, R.anim.gallery_description_show);
-            animation.setAnimationListener(new Animation.AnimationListener() {
-                @Override
-                public void onAnimationStart(Animation animation) {
-                    tvImageDescription.setVisibility(View.VISIBLE);
-                }
-
-                @Override
-                public void onAnimationEnd(Animation animation) {
-
-                }
-
-                @Override
-                public void onAnimationRepeat(Animation animation) {
-
-                }
-            });
-            tvImageDescription.startAnimation(animation);
-
-        }
-
     }
-    private  String getData(String sourceStr,String pattern){
+
+    private String getData(String sourceStr, String pattern) {
         //  String filetext = "张小名=25分|李小花=43分|王力=100分|";
         Pattern p = Pattern.compile(pattern);//正则表达式，取=和|之间的字符串，不包括=和|
         Matcher m = p.matcher(sourceStr);
-        if(m.find()){
+        if (m.find()) {
             return m.group(1);
-        }else{
+        } else {
             return "";
         }
     }
+
     private void shiTu(String imageURL) {
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
-
         try {
-            String apiUrl = "http://stu.baidu.com/i?objurl="+imageURL+"&filename=&rt=0&rn=10&ftn=searchstu&ct=1&stt=1&tn=faceresult";
-
+            String apiUrl = "http://stu.baidu.com/i?objurl=" + imageURL + "&filename=&rt=0&rn=10&ftn=searchstu&ct=1&stt=1&tn=faceresult";
             asyncHttpClient.get(apiUrl, new AsyncHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers,
                                       byte[] responseBody) {
-                    String data=new String(responseBody);
+                    String data = new String(responseBody);
                     Log.i("SHITU", data);
-                    String name=getData(data,"keyword:BD.STU.escapeXSS\\(\\\"(.*)\\\"\\),keywordEnc");
-                    String baikeUrl=getData(data,"baikeURL:\\\"(.*)\\\",baikeText");
-                    String baikeText=getData(data,"baikeText:\\\"(.*)\\\",baikeTitle");
+                    String name = getData(data, "keyword:BD.STU.escapeXSS\\(\\\"(.*)\\\"\\),keywordEnc");
+                    String baikeUrl = getData(data, "baikeURL:\\\"(.*)\\\",baikeText");
+                    String baikeText = getData(data, "baikeText:\\\"(.*)\\\",baikeTitle");
                     Log.i("SHITU", name);
                     Log.i("SHITU", baikeUrl);
                     Log.i("SHITU", baikeText);
+                    showShiTuResult(name, baikeText, baikeUrl);
                 }
 
                 @Override
@@ -167,7 +137,52 @@ public class GalleryActivity extends Activity {
             });
         } catch (Exception e) {
         }
+    }
 
+    private void showShiTuResult(final String name, final String baikeText, final String baikeUrl) {
+        final AlertDialog dlg = new AlertDialog.Builder(this).create();
+        dlg.show();
+        dlg.setCanceledOnTouchOutside(true);
+        Window window = dlg.getWindow();
+        // *** 主要就是在这里实现这种效果的.
+        // 设置窗口的内容页面,shrew_exit_dialog.xml文件中定义view内容
+        window.setContentView(R.layout.pop_shitu);
 
+        LinearLayout shiTuResultLayout = (LinearLayout) window.findViewById(R.id.shiTuResultLayout);
+        String[] results = name.split("\\*");
+
+        for (int i = 0; i < results.length; i++) {
+            final String lname=results[i];
+            TextView tvName = new TextView(this);
+            tvName.setText(" "+lname+" ");
+            tvName.setTextSize(25);
+
+            tvName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        String url = "http://image.baidu.com/i?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&word=" + URLEncoder.encode(lname, "utf-8");
+                        Uri uri = Uri.parse(url);
+                        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(it);
+
+                    } catch (Exception e) {
+
+                    }
+                }
+            });
+
+            shiTuResultLayout.addView(tvName);
+        }
+        TextView tvBaiKeText = (TextView) window.findViewById(R.id.tvBaiKeText);
+        tvBaiKeText.setText(baikeText);
+        tvBaiKeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(baikeUrl);
+                Intent it = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(it);
+            }
+        });
     }
 }
